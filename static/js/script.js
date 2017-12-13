3921abaff3cfc3eac6faeec4aebcfb1e511628e9
bezgodoy <belisa.godoy@gmail.com>
2017-12-13 12:07:20 -0200

    hello popo2

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+// window.onload = function() {
+//     alert('oi');
+//     // ação pra quando o site abrir
+// }
+
+
+
+// // função abstrata = descartável
+// // onde? quando? o que?
+
+
+// var imprimiTeste = function() {
+//     alert('oi');
+// }
+
+$(".rectangle2").hover(
+    function() {
+        $(this).addClass("rectangle2-cover");
+    }, function() {
+        $(this).removeClass("rectangle2-cover");
+    }
+);
